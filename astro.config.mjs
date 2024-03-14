55bf9d8aa6317917ff2331ae588d556377842c14
Lukasz Tyszkiewicz <lukasz@airfocus.com>
2024-03-14 11:28:28 +0000

    feat: try with storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,19 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
-import compress from 'astro-compress';
-import icon from 'astro-icon';
-import sanity from '@sanity/astro';
-
-import react from '@astrojs/react';
-
-import { loadEnv } from "vite";
-const { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
-// Different environments use different variables
-const projectId = PUBLIC_SANITY_PROJECT_ID
-const dataset = PUBLIC_SANITY_DATASET
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+import netlify from "@astrojs/netlify";
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
-  compressHTML: true,
-  integrations: [
-    mdx(),
-    icon(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    compress(),
-    sanity({
-      projectId,
-      dataset,
-      useCdn: false,
-      studioBasePath: '/studio',
-      apiVersion: "2024-03-01",
-    }),
-    react(),
-  ],
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      blogPost: 'storyblok/BlogPost',
+      blogPostList: 'storyblok/BlogPostList',
+      page: 'storyblok/Page'
+    }
+  })],
+  output: "server",
+  adapter: netlify()
 });
